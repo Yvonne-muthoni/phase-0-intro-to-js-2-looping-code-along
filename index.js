47f0e40b1d66008ef4b2d34ee41188ee27551c4a
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-04-03 21:29:05 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+ function countDown(){
+    let i = 0;
+    while(i<11){
+        console.log (i++);
+    }
+    
+ }
+ writeCards (["Guadalupe", "Ollie", "Aki"],'surprise');
+ function writeCards(names) {
+  for (let i = 3; i < names.length;i++) {
+    console.log(`Thank you,${names[i]}, for the wonderful surprise gift!`);
+  }
+  return writeCards;
+}
